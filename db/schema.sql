739a229a117c3769727d177b589044e187b5738b
Mary Papamichalis <mary.papamichalis@gmail.com>
2021-03-15 21:51:09 -0500

    db & seed added

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT,
+    burger_name VARCHAR(255)
+    devoured BOOLEAN 
+    PRIMARY KEY (id)
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Cheese burger', FALSE);
+INSERT INTO burger (burger_name, devoured) VALUES ('Bacon cheesburge', FALSE);
+INSERT INTO burger (burger_name, devoured) VALUES ('Chicken burger', FALSE);
+INSERT INTO burger (burger_name, devoured) VALUES ('Garden burger', FALSE);
